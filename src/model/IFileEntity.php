9e77bdf406c77fa6a4ddc588e1addee27098bda1
Vaclav Sir <vaclav.sir@gmail.com>
2013-10-04 15:37:29 +0200

    Describe entity methods.

--- src/model/IFileEntity.php
+++ src/model/IFileEntity.php
@@ -6,13 +6,43 @@ interface IFileEntity
 {
 
 	/**
+	 * ID of entity, if it's already persisted, or NULL.
+	 *
 	 * @return int|NULL
 	 */
 	public function getId();
+
+	/**
+	 * User file name.
+	 *
+	 * @return string
+	 */
 	public function getFileName();
+
+	/**
+	 * @return int
+	 */
 	public function getFileSize();
+
+	/**
+	 * MIME content type.
+	 *
+	 * @return string
+	 */
 	public function getContentType();
+
+	/**
+	 * File name extension.
+	 *
+	 * @return string
+	 */
 	public function getExtension();
+
+	/**
+	 * Full path to the file on filesystem.
+	 *
+	 * @return string
+	 */
 	public function getFullPath();
 
 }
